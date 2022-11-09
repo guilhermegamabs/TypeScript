816251d05de87b205f4f633c011fc0546c5e2adb
Guilherme Gama <guilhermegamabs@gmail.com>
2022-11-09 12:28:20 -0300

    Enum

---
+++ src/A009-type-enum/A009-type-enum.ts
@@ -0,0 +1,22 @@
+enum Colors {
+  RED = 10, // 10
+  BLUE = 21, // 21
+  YELLOW = 32, // 32
+}
+
+enum Colors {
+  GREEN = 'GREEN',
+  ORANGE = 222,
+  PINK,
+}
+
+function chooseColor(cor: Colors): void {
+  console.log(Colors[cor]);
+}
+
+chooseColor(Colors.PINK);
+
+console.log(Colors);
+// console.log(Colors.RED);
+// console.log(Colors[10]);
+// console.log(Colors.GREEN);
