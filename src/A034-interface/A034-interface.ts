6d1d16176562f5baf6c6608b4869d3d68f0b3c31
Guilherme Gama <guilhermegamabs@gmail.com>
2022-11-21 19:54:16 -0300

    Exercise

---
+++ src/A034-interface/A034-interface.ts
@@ -0,0 +1,33 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'Guilherme',
+  sobrenome: 'Gama',
+};
+
+const pessoa = new Pessoa('Guilherme', 'Gama');
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
