d720ffc13fec3220a70a6029e085a839e055c733
Guilherme Gama <guilhermegamabs@gmail.com>
2022-11-21 20:00:02 -0300

    Declaration merging

---
+++ src/A036-merging-interfaces/A036-merging-interfaces.ts
@@ -0,0 +1,24 @@
+interface Pessoa {
+  nome: string;
+}
+
+interface Pessoa {
+  readonly sobrenome: string;
+}
+
+interface Pessoa {
+  readonly enderecos: readonly string[];
+}
+
+interface Pessoa {
+  readonly idade?: number;
+}
+
+const pessoa1: Pessoa = {
+  nome: 'Guilherme',
+  sobrenome: 'Gama',
+  enderecos: ['Av.Santo Amaro'],
+  idade: 18,
+};
+
+console.log(pessoa1);
