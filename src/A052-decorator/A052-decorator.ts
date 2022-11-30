c6838f0a0a69e501c154a539c492b9cc1aa9d174
Guilherme Gama <guilhermegamabs@gmail.com>
2022-11-30 17:12:47 -0300

    Decorator

---
+++ src/A052-decorator/A052-decorator.ts
@@ -0,0 +1,23 @@
+@decorator
+export class Animal {
+  constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    nome: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this.cor = this.inverte(args[1]);
+      this.nome = this.inverte(args[0]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('Gui', 'roxo');
+console.log(animal);
